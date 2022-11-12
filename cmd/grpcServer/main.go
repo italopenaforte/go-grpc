b44024a84d7c0a0bf9fe2b963649c87c1b7a500f
Italo Rangel <italopenaforte@gmail.com>
2022-11-12 19:57:24 -0300

    initial commit

---
+++ cmd/grpcServer/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"database/sql"
+	"net"
+
+	"github.com/italopenaforte/go-grpc/internal/database"
+	"github.com/italopenaforte/go-grpc/internal/pb"
+	"github.com/italopenaforte/go-grpc/internal/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDB)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	lis, err := net.Listen("tcp", ":50051")
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
---
+++ internal/service/category.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"context"
+
+	"github.com/italopenaforte/go-grpc/internal/database"
+	"github.com/italopenaforte/go-grpc/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDB,
+	}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.Category, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return categoryResponse, nil
+}
